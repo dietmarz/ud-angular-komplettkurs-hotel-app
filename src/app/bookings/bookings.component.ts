b8e210212de1324e594fe6febee3f91ab588506d
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-01-19 15:43:42 +0100

    2.11

---
+++ src/app/bookings/bookings.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css']
+})
+export class BookingsComponent implements OnInit {
+
+  constructor() { }
+
+  booking = "Dietmars Buchung"
+
+  ngOnInit(): void {
+  }
+
+}
