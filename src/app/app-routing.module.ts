45463c6b15c58a31b1d24501bae5b0fbfa246b1d
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-02-06 17:40:14 +0100

    3.16
    ng generate module app-routing --flat --module=app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {BookingsComponent} from "./bookings/bookings.component";
+
+
+const routes: Routes = [
+  {
+    path:'bookings', component: BookingsComponent
+  }
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+    // CommonModule
+  ], exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
