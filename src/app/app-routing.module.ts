819ef0b2de837e444b6974245e9b6e2adf3c0d01
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-02-13 10:46:06 +0100

    4.19 add routing to createBooking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {BookingsComponent} from "./bookings/bookings.component";
+import {CreateBookingComponent} from "./create-booking/create-booking.component";
 
 
 const routes: Routes = [
   {
     path:'bookings', component: BookingsComponent
+  },
+  {
+    path:'createBooking', component: CreateBookingComponent
   },
   {
     path:'', redirectTo:'bookings', pathMatch: 'full'
